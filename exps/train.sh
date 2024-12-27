ae73c9ee9a41bc800ae8b19a0a31ace04c6c9dc2
Sethi Harshit <sethih1@login4.triton.aalto.fi>
2024-12-27 23:21:54 +0200

    Updated training configurations according to needs

--- exps/train.sh
+++ exps/train.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 NUM_NODES=1
-NUM_GPUS_PER_NODE=2
+NUM_GPUS_PER_NODE=8
 NODE_RANK=0
 BATCH_SIZE=64
 ACCUM_STEP=1
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
 DATESTR=$(date +"%m-%d-%H-%M")
 
-SAVE_PATH=/
+SAVE_PATH=/home/sethih1/MolNexTR/exps/logs/
 
 mkdir -p ${SAVE_PATH}
 
@@ -17,11 +17,11 @@ set -x
 torchrun \
     --nproc_per_node=$NUM_GPUS_PER_NODE --nnodes=$NUM_NODES --node_rank $NODE_RANK --master_addr localhost --master_port $MASTER_PORT \
     main.py \
-    --data_path data \
-    --train_file pubchem/train_pubchem.csv \
-    --aux_file uspto/train_uspto.csv --coords_file aux_file \
-    --valid_file real/acs.csv \
-    --test_file real/your_data_path \
+    --data_path /scratch/phys/sin/sethih1/molnext_data_file \
+    --train_file /scratch/phys/sin/sethih1/molnext_data_file/train_pubchem.csv \
+    --aux_file /scratch/phys/sin/sethih1/molnext_data_file/uspto_mol/train_uspto.csv --coords_file aux_file \
+    --valid_file /scratch/phys/sin/sethih1/molnext_data_file/real/acs.csv \
+    --test_file /scratch/phys/sin/sethih1/molnext_data_file/real/acs.csv \
     --vocab_file MolNexTR/vocab/vocab_chars.json \
     --formats chartok_coords,edges \
     --dynamic_indigo --augment --mol_augment \
@@ -32,7 +32,7 @@ torchrun \
     --decoder_lr 4e-4 \
     --save_path $SAVE_PATH --save_mode all \
     --label_smoothing 0.1 \
-    --epochs 40 \
+    --epochs 20 \
     --batch_size $((BATCH_SIZE / NUM_GPUS_PER_NODE / ACCUM_STEP)) \
     --gradient_accumulation_steps $ACCUM_STEP \
     --use_checkpoint \
